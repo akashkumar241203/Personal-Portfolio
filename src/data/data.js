5c0649a053c6ab9344bbcbe1596390da19575f86
Akash Kumar <akashkumar.psl24@gmail.com>
2024-10-14 14:52:57 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,36 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: "https://i.postimg.cc/x1M71VVN/Screenshot-2024-09-03-174725.png",
+    title: "Animated_Card(ReactJs)",
+    desc: "Experience the magic of ReactJS animations with this engaging carousel! The website features a sleek, interactive carousel showcasing smooth transitions and dynamic content, highlighting the creative possibilities of ReactJS in modern web development.",
+    link: "https://zesty-toffee-f6ce89.netlify.app/",
+    git: "https://github.com/akashkumar241203/Animated_card-ReactJs-",
+  },
+  {
+    id: 2,
+    img: "https://i.postimg.cc/JzR6M6MR/Screenshot-2024-09-03-185408.png",
+    title: "Blog Website",
+    desc: "A full-stack web app using ReactJS, Tailwind CSS, ExpressJS, and MongoDB. Features include user authentication and CRUD functionality for creating, editing, and deleting posts, with the ability to view posts by other users.",
+    link: "",
+    git: "https://github.com/akashkumar241203/Bloging-site",
+  },
+  {
+    id: 3,
+    img: "https://i.postimg.cc/ZKFx0h0n/Screenshot-2024-10-10-172449.png",
+    title: "Zerodha Clone",
+    desc: "The Zerodha Clone is a stock trading platform built with the MERN stack, featuring real-time market data, stock trading operations, and portfolio management. It integrates Chart.js to provide interactive charts for visualizing stock price movements and performance trends, giving users insights into their investments. The project includes user authentication, secure APIs, and a responsive design, simulating the experience of a real stock trading platform.",
+    link: "https://zerodha-frontend-1h04.onrender.com/",
+    git: "https://github.com/akashkumar241203/Zerodha-Clone",
+  },
+  {
+    id: 4,
+    img: "https://i.postimg.cc/hjmrpRjF/Screenshot-2024-10-10-163834.png",
+    title: "TravelNest",
+    desc: "TravelNest is a web platform built using MongoDB, Express, Node.js, EJS, and MapBox that enhances travel planning and exploration. It allows users to discover destinations, create custom itineraries, and manage bookings seamlessly. With an intuitive interface and MapBox integration for real-time map data, users can explore routes, attractions, and accommodations with ease, making TravelNest an all-in-one travel companion.",
+    link: "https://travelnest-uhuf.onrender.com/listings/",
+    git:  "https://github.com/akashkumar241203/TravelNest",
+  },
+];
